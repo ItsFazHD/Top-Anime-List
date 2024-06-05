ac76841d98b015f1ae5ff2894d96ab35f1fab80e
Faisal Alam <fazhd@hotmail.com>
2024-06-05 13:29:09 +0100

    starting the first catergory

--- top-anime-list/top-anime-list/src/App.jsx
+++ top-anime-list/top-anime-list/src/App.jsx
@@ -1,10 +1,11 @@
 import Header from "./Header";
+
 import Footer from "./Footer";
 function App() {
   return (
     <>
       <Header />
-    
+      <div>Top 5 Romance Anime:</div>
     </>
   );
 }
--- top-anime-list/top-anime-list/src/Footer.jsx
+++ top-anime-list/top-anime-list/src/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <>
-      <footer className="text-center text-sm font-Reenie Beanie bg-indigo-800 text-gray-200 p-6">
+      <footer className="text-3xl font-Reenie Beanie text-gray-200 p-6 flex justify-end">
         Created by FazHD
       </footer>
     </>
