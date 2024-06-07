0a35220b918d32dcdf8d98bee5fe8a1af260d16e
Faisal Alam <fazhd@hotmail.com>
2024-06-07 01:35:39 +0100

    added anime name:

--- top-anime-list/top-anime-list/src/App.jsx
+++ top-anime-list/top-anime-list/src/App.jsx
@@ -94,7 +94,7 @@ function App() {
   const images3 = [
     {
       src: "images/slice of life/mr villain.png",
-      description: "Mr. Villain's day off",
+      description: "Anime Name: Mr. Villain's day off",
       descriptionText:
         "As a member of a malevolent organization, the General is tasked with invading Earth and wiping out humanity. Even an extraterrestrial being like him, however, needs a chance to rest. Not even the Rangers—a team solely dedicated to defeating the General and his colleagues—will stand in the way of him visiting pandas at the zoo, buying ice cream at the convenience store, and enjoying his well-deserved day off from committing evil deeds.",
       objectPosition: "center",
@@ -102,7 +102,7 @@ function App() {
 
     {
       src: "images/slice of life/spy x family.jpeg",
-      description: "Spy X Family",
+      description: "Anime Name: Spy X Family",
       descriptionText:
         "For the agent known as Twilight, no order is too tall if it is for the sake of peace. Operating as Westalis' master spy, Twilight works tirelessly to prevent extremists from sparking a war with neighboring country Ostania. For his latest mission, he must investigate Ostanian politician Donovan Desmond by infiltrating his son's school: the prestigious Eden Academy. Thus, the agent faces the most difficult task of his career: get married, have a child, and play family. Twilight, or Loid Forger, quickly adopts the unassuming orphan Anya to play the role of a six-year-old daughter and prospective Eden Academy student. For a wife, he comes across Yor Briar, an absent-minded office worker who needs a pretend partner of her own to impress her friends. However, Loid is not the only one with a hidden nature. Yor moonlights as the lethal assassin Thorn Princess. For her, marrying Loid creates the perfect cover. Meanwhile, Anya is not the ordinary girl she appears to be; she is an esper, the product of secret experiments that allow her to read minds. Although she uncovers their true identities, Anya is thrilled that her new parents are cool secret agents! She would never tell them, of course. That would ruin the fun. Under the guise of The Forgers, the spy, the assassin, and the esper must act as a family while carrying out their own agendas. Although these liars and misfits are only playing parts, they soon find that family is about far more than blood relations. ",
       objectPosition: "center top",
@@ -110,7 +110,7 @@ function App() {
 
     {
       src: "images/slice of life/guide to babysitting.png",
-      description: "The Yakuza's Guide To Babysitting",
+      description: "Anime Name: The Yakuza's Guide To Babysitting",
       descriptionText:
         "Tooru Kirishima's notoriety is spread far and wide in the underworld. He is most commonly known as The Demon of Sakuragi—a man who is not afraid to resort to violence if deemed necessary. After almost jeopardizing a peace treaty, his boss tasks him with the most difficult job he has ever had: taking care of seven-year-old Yaeka Sakuragi—the boss' precious daughter—so that Tooru understands what it means to be responsible for another life. At first, the two do not seem to meet eye to eye, as Tooru has no clue on how to communicate with Yaeka, and the young girl is not used to expressing her emotions. However, as time goes on, they come to understand each other despite their differences. The fearsome right-hand man of a yakuza boss and the child he must protect are about to learn that family is not always bound by blood.",
       objectPosition: "center top",
@@ -118,7 +118,7 @@ function App() {
 
     {
       src: "images/slice of life/househusband.png",
-      description: "The Way Of The Househusband",
+      description: "Anime Name: The Way Of The Househusband",
       descriptionText:
         "Immortal Tatsu, the legendary yakuza who single-handedly defeated a rival gang with a lead pipe, is a name known to strike fear in both hardened police officers and vicious criminals. Soon after his sudden disappearance, he resurfaces with a slight change in profession. Now equipped with an apron, Tatsu has given up violence and is trying to make an honest living as a house husband. While adapting to mundane household tasks, Tatsu finds that being a house husband has its own challenges, from the battlefield known as supermarket sales to failures in the kitchen. Despite living peacefully, misunderstandings seem to follow him left and right. Gokushufudou follows the daily life of the comically serious ex-yakuza as he leaves behind his dangerous previous life to become a stay-at-home husband.",
       objectPosition: "center top",
@@ -126,7 +126,7 @@ function App() {
 
     {
       src: "images/slice of life/bunny drop.png",
-      description: "Bunny Drop",
+      description: "Anime Name: Bunny Drop",
       descriptionText:
         "Daikichi Kawachi is a 30-year-old bachelor working a respectable job but otherwise wandering aimlessly through life. When his grandfather suddenly passes away, he returns to the family home to pay his respects. Upon arriving at the house, he meets a mysterious young girl named Rin who, to Daikichi’s astonishment, is his grandfather's illegitimate daughter! The shy and unapproachable girl is deemed an embarrassment to the family, and finds herself ostracized by her father's relatives, all of them refusing to take care of her in the wake of his death. Daikichi, angered by their coldness towards Rin, announces that he will take her in—despite the fact that he is a young, single man with no prior childcare experience. Usagi Drop is the story of Daikichi's journey through fatherhood as he raises Rin with his gentle and affectionate nature, as well as an exploration of the warmth and interdependence that are at the heart of a happy, close-knit family.",
       objectPosition: "center top",
@@ -136,7 +136,7 @@ function App() {
   const images4 = [
     {
       src: "images/shounen/dbz.jpg",
-      description: "Dragon Ball Z",
+      description: "Anime Name: Dragon Ball Z",
       descriptionText:
         "Five years after winning the World Martial Arts tournament, SON-Goku is now living a peaceful life with his wife and son. This changes, however, with the arrival of a mysterious enemy named Raditz who presents himself as Goku's long-lost brother. He reveals that Goku is a warrior from the once powerful but now virtually extinct Saiyan race, whose homeworld was completely annihilated. When he was sent to Earth as a baby, Goku's sole purpose was to conquer and destroy the planet; but after suffering amnesia from a head injury, his violent and savage nature changed, and instead was raised as a kind and well-mannered boy, now fighting to protect others. With his failed attempt at forcibly recruiting Goku as an ally, Raditz warns Goku's friends of a new threat that's rapidly approaching Earth—one that could plunge Earth into an intergalactic conflict and cause the heavens themselves to shake. A war will be fought over the seven mystical dragon balls, and only the strongest will survive in Dragon Ball Z.",
       objectPosition: "center top",
@@ -144,7 +144,7 @@ function App() {
 
     {
       src: "images/shounen/jjk.jpeg",
-      description: "Jujutsu Kaisen",
+      description: "Anime Name: Jujutsu Kaisen",
       descriptionText:
         "Idly indulging in baseless paranormal activities with the Occult Club, high schooler Yuuji Itadori spends his days at either the clubroom or the hospital, where he visits his bedridden grandfather. However, this leisurely lifestyle soon takes a turn for the strange when he unknowingly encounters a cursed item. Triggering a chain of supernatural occurrences, Yuuji finds himself suddenly thrust into the world of Curses—dreadful beings formed from human malice and negativity—after swallowing the said item, revealed to be a finger belonging to the demon Sukuna Ryoumen, the King of Curses. Yuuji experiences first-hand the threat these Curses pose to society as he discovers his own newfound powers. Introduced to the Tokyo Metropolitan Jujutsu Technical High School, he begins to walk down a path from which he cannot return—the path of a Jujutsu sorcerer.",
       objectPosition: "center top",
@@ -152,7 +152,7 @@ function App() {
 
     {
       src: "images/shounen/mashle.jpeg",
-      description: "Mashle: Magic and Muscles",
+      description: "Anime Name: Mashle: Magic and Muscles",
       descriptionText:
         "In this magical world, one is easily identified as having magical abilities by a distinctive mark on their face. Those unable to practice magic are swiftly exterminated to maintain the magical integrity of society. However, deep within a forest lies an anomaly in Mash Burnedead, who can be found pumping iron with one arm and lifting a cream puff with the other. This aloof boy with superhuman strength—but no magical abilities—leads a quiet life with his father, far removed from society. Mash's peace is soon disturbed when the authorities discover his lack of magical powers. They issue him an ultimatum: compete to become a Divine Visionary, which would force everyone to accept him, or be persecuted forever. To protect his family, he enrolls in the prestigious Easton Magic Academy, which only the most elite and gifted students are allowed to attend. Now, Mash must overcome his shortcomings as a magicless being and surpass the other students—relying solely on his muscles.",
       objectPosition: "center top",
@@ -160,7 +160,7 @@ function App() {
 
     {
       src: "images/shounen/aot.jpg",
-      description: "Attack On Titan",
+      description: "Anime Name: Attack On Titan",
       descriptionText:
         "Centuries ago, mankind was slaughtered to near extinction by monstrous humanoid creatures called titans, forcing humans to hide in fear behind enormous concentric walls. What makes these giants truly terrifying is that their taste for human flesh is not born out of hunger but what appears to be out of pleasure. To ensure their survival, the remnants of humanity began living within defensive barriers, resulting in one hundred years without a single titan encounter. However, that fragile calm is soon shattered when a colossal titan manages to breach the supposedly impregnable outer wall, reigniting the fight for survival against the man-eating abominations. After witnessing a horrific personal loss at the hands of the invading creatures, Eren Yeager dedicates his life to their eradication by enlisting into the Survey Corps, an elite military unit that combats the merciless humanoids outside the protection of the walls. Based on Hajime Isayama's award-winning manga, Shingeki no Kyojin follows Eren, along with his adopted sister Mikasa Ackerman and his childhood friend Armin Arlert, as they join the brutal war against the titans and race to discover a way of defeating them before the last walls are breached.",
       objectPosition: "center top",
@@ -168,7 +168,7 @@ function App() {
 
     {
       src: "images/shounen/hells paradise.jpeg",
-      description: "Hell's Paradise",
+      description: "Anime Name: Hell's Paradise",
       descriptionText:
         "Sentenced to death, ninja Gabimaru the Hollow finds himself apathetic. After leading a blood-soaked life, Gabimaru believes he deserves to die. However, every attempt to execute him inexplicably fails. Finally, Sagiri Yamada Asaemon, a fledgling member of a famed executioner clan, is asked to take Gabimaru's life; yet Sagiri makes no move to kill him as requested. Insisting that Gabimaru will not die because of his love for his wife, Sagiri instead offers him the chance to obtain a full pardon for his crimes. If he can travel to the island of Shinsekyo and obtain the Elixir of Life—which supposedly grants immortality—and bring it back for the shogun, then his freedom will be assured. But of the many who have traveled to Shinsekyo in search of the mythical Elixir, not a single person has returned sound of mind, if at all. Though unaware of the numerous dangers ahead, Gabimaru decides to accept the offer—alongside ten other death row convicts—in hope that he and his wife may finally live in peace.",
       objectPosition: "center top",
@@ -178,7 +178,7 @@ function App() {
   const images5 = [
     {
       src: "images/comedy/aho girl.png",
-      description: "Aho-Girl",
+      description: "Anime Name: Aho-Girl",
       descriptionText:
         "Yoshiko Hanabatake is an idiot beyond all belief. Somehow managing to consistently score zeroes on all of her tests and consumed by an absurd obsession with bananas, her senseless acts have caused even her own mother to lose all hope. Only one person is up to the task of keeping her insanity in check: childhood friend Akuru A-kun Akutsu. Though he bemoans the ridiculous behavior he has to endure, the studious but terrifying A-kun is always ready to put an end to any stupidity Yoshiko gets up to, with no qualms about using physical force. Unfortunately, no matter how many times he attempts to knock some sense into her, the girl bounces right back to her usual shenanigans, even dragging in some other eccentrics along for the ride. Try as he might to rein in her nonsense, every moment is unpredictable with Yoshiko and her profound idiocy on the loose. ",
       objectPosition: "center bottom",
@@ -186,7 +186,7 @@ function App() {
 
     {
       src: "images/comedy/himouto.jpg",
-      description: "Himouto! Umaru-chan",
+      description: "Anime Name: Himouto! Umaru-chan",
       descriptionText:
         "People are not always who they appear to be, as is the case with Umaru Doma, the perfect high school girl—that is, until she gets home! Once the front door closes, the real fun begins. When she dons her hamster hoodie, she transforms from a refined, over-achieving student into a lazy, junk food-eating otaku, leaving all the housework to her responsible older brother Taihei. Whether she's hanging out with her friends Nana Ebina and Kirie Motoba, or competing with her self-proclaimed rival Sylphinford Tachibana, Umaru knows how to kick back and have some fun! Himouto! Umaru-chan is a cute story that follows the daily adventures of Umaru and Taihei, as they take care of—and put up with—each other the best they can, as well as the unbreakable bonds between friends and siblings.",
       objectPosition: "center",
@@ -194,7 +194,7 @@ function App() {
 
     {
       src: "images/comedy/toture princess.png",
-      description: "Tis Time for Torture, Princess",
+      description: "Anime Name: Tis Time for Torture, Princess",
       descriptionText:
         "The princess who's been held captive by the Demon Lord's army is scheduled to be executed by the Demon Lord's relentless torture!",
       objectPosition: "center",
@@ -202,7 +202,7 @@ function App() {
 
     {
       src: "images/comedy/monster girls.png",
-      description: "Interviews With Monster Girls",
+      description: "Anime Name: Interviews With Monster Girls",
       descriptionText:
         "High school biology teacher Tetsuo Takahashi may look like your average everyday instructor, but beneath his gentle appearance lies something less ordinary: his fascination for the Ajin, more commonly known as Demi. Although these half-human, half-monster beings have integrated into human society, Takahashi believes that much about them will remain unknown unless he interacts with them firsthand. Demi-chan wa Kataritai follows Takahashi's daily life in Shibasaki High School together with his three Demi students—Hikari Takanashi, an energetic vampire; Kyouko Machi, a gentle dullahan; and Yuki Kusakabe, the shy snow woman. Along the way, Takahashi also meets fellow teacher Sakie Satou, a succubus with an aversion towards men. To fulfill his goal of learning more about the Demi, Takahashi decides to conduct casual interviews with the girls to learn more about their abilities, psyche, and interaction with human society. As Takahashi strengthens his bond with his students, he soon discovers that the Demi are not as unusual as he initially believed.",
       objectPosition: "center top",
@@ -210,7 +210,7 @@ function App() {
 
     {
       src: "images/comedy/cool guys.jpg",
-      description: "Play It Cool, Guys",
+      description: "Anime Name: Play It Cool, Guys",
       descriptionText:
         "Despite their distinctive personalities, Souma Shiki, Hayate Ichikura, Shun Futami, and Takayuki Mima all have one thing in common: though naturally clumsy, the four disguise their embarrassment from tiny slip-ups by maintaining a composed demeanor. However, it is actually the guys' airheaded natures that makes the girls' hearts throb. No matter what happens in their daily lives, the boys do their best not to lose their cool! ",
       objectPosition: "center top",
