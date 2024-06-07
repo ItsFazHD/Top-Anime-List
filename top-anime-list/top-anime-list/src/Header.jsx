a5599720c7d384bfd7157e5475dd61bde087dc4a
Faisal Alam <fazhd@hotmail.com>
2024-06-07 02:19:54 +0100

    removed the search

--- top-anime-list/top-anime-list/src/Header.jsx
+++ top-anime-list/top-anime-list/src/Header.jsx
@@ -3,11 +3,12 @@ function Header() {
     <>
       <header className="text-start text-6xl font-Fuzzy font-bold text-gray-200 p-6 bg-indigo-800">
         FazHD&apos;s Top Anime List
-        <div className="flex justify-end">
-          <textarea
+        <div className="flex justify-end"></div>
+        {/*          <textarea
             className="bg-transparent border w-80 h-8 text-sm"
             name="search"
-            id="search"></textarea>
+            id="search"
+            placeholder="Search"></textarea>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -20,8 +21,7 @@ function Header() {
               strokeLinejoin="round"
               d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
             />
-          </svg>
-        </div>
+          </svg> */}
       </header>
     </>
   );
